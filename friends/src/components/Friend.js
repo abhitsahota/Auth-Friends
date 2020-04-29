5929dbfd1eacdf83787a9f3c4f296b6081650061
abhitsahota <abhit.sahota@gmail.com>
2020-04-28 21:59:00 -0400

    completed MVP

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -3,6 +3,12 @@ import { axiosAuth } from '../utils/axiosAuth'
 
 export const Friend = () => {
 
+    const [ newFriend, setNewFriend ] = useState({
+        name: '',
+        age: '',
+        email: ''
+    })
+
     const [ data, setData ] = useState([])
     
     useEffect(() => {
@@ -14,10 +20,57 @@ export const Friend = () => {
         .catch(err => console.log(err))
     }, [])
 
-    console.log(data)
+    const handleChange = e => {
+        setNewFriend({
+            ...newFriend,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const friendAdd = {...newFriend, id: data.length+1 }
+        axiosAuth()
+        .post('/friends', friendAdd)
+        .then(console.log(data))
+        .catch(err => console.log(err))
+
+        setNewFriend({
+            id: '',
+            name: '',
+            age: '',
+            email: ''
+        })
+        window.location.reload();
+    }
 
     return (
         <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='text'
+                    name='name'
+                    value={newFriend.name}
+                    placeholder='Name of your friend'
+                    onChange={handleChange}
+                />
+                <input 
+                    type='number'
+                    name='age'
+                    value={newFriend.age}
+                    placeholder='Age of your friend'
+                    onChange={handleChange}
+                />
+                <input 
+                    type='email'
+                    name='email'
+                    value={newFriend.email}
+                    placeholder='Email of your friend'
+                    onChange={handleChange}
+                />
+                <button>Add!</button>
+            </form>
+
             <ul>
                 {data.map(friend => {
                     return (
